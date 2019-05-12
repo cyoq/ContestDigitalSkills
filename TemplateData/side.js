b1364048904a53e26623dc31adde089ad2b418f4
Alex <di9akoz@gmail.com>
2019-05-12 23:18:50 +0300

    adding content

--- TemplateData/side.js
+++ TemplateData/side.js
@@ -1,6 +1,7 @@
+var gameInstance;
 var btn = document.getElementsByClassName("btn")[0]; 
 
+
 btn.onclick = () => {
-    var gameInstance = UnityLoader.instantiate("gameContainer", "Build/Build_WebGL.json", {onProgress: UnityProgress});
-    btn.classList.add("hidden");
+    gameInstance = UnityLoader.instantiate("gameContainer", "Build/Build_WebGL.json", {onProgress: UnityProgress});
 };
